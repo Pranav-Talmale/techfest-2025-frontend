d9d94a703a7ce2e606f705a14cc3954e33c41535
Pranav-Talmale <pranavtalmale@gmail.com>
2025-02-23 13:31:54 +0530

    Add 404 error page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "@/pages/Home";
+import ErrorPage from "@/pages/ErrorPage";
 import SidebarComponent from "@/components/Sidebar/SidebarComponent";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <Routes>
       <Route path="/" element={<SidebarComponent />}>
         <Route index element={<Home />} />
+        <Route path="*" element={<ErrorPage />} />
       </Route>
     </Routes>
   );
